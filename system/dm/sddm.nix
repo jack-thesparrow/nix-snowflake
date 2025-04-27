e842390b73473e45f002139083381487941eb051
jack-thesparrow <rahultudu4705@gmail.com>
2025-04-27 23:06:56 +0530

    added gdm

--- system/dm/sddm.nix
+++ system/dm/sddm.nix
@@ -3,7 +3,5 @@
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.displayManager.gdm.wayland = true;
   services.displayManager.defaultSession = "hyprland";
-  environment.systemPackages = with pkgs; [
-    hyprland.packages.${pkgs.system}.hyprland
-  ];
+  programs.hyprland.enable = true;
 }
--- system/wm/hyprland.nix
+++ system/wm/hyprland.nix
@@ -1,11 +1,11 @@
-{ config, pkgs, lib, ... }:
+{ config, pkgs, lib, hyprland, ... }:
 {
   options = {
     snowflake.hyprland.enable = lib.mkEnableOption "Enable Hyprland WM";
   };
 
   config = lib.mkIf config.snowflake.hyprland.enable {
-    environment.systemPackages = with pkgs; [
+    environment.systemPackages = [
       hyprland
     ];
   };
